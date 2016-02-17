9f3eb24761e0305b5bb030f005d84233bfe3bcd0
Terrence Lam <skyuplam@gmail.com>
2016-02-17 18:17:32 +0800

    Added new dependencies - helmt, for security - passport, auth - passport-local, auth

--- src/server/main.js
+++ src/server/main.js
@@ -2,9 +2,13 @@ import api from './api';
 import config from './config';
 import errorHandler from './lib/errorHandler';
 import express from 'express';
+import helmet from 'helmet';
 import frontend from './frontend';
 
 const app = express();
+
+// helmet
+app.use(helmet());
 
 app.use('/api/v1', api);
 app.use(frontend);
