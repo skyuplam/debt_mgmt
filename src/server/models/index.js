c7da34655a6a9d4359c199c6a8411a016343d4c8
Terrence Lam <skyuplam@gmail.com>
2016-03-09 09:47:29 +0800

    fixed config bug introduced by last merge

--- src/server/models/index.js
+++ src/server/models/index.js
@@ -1,7 +1,7 @@
 import Sequelize from 'sequelize';
 import fs from 'fs';
 import path from 'path';
-import config from '../config';
+import config from '../../common/config';
 
 const env = process.env.NODE_ENV || 'development';
 const dbConfig = config.db[env];
